5114bd11868c6df659d09739716dbccd34b74596
jcha0713 <joocha0713@gmail.com>
2022-05-04 23:24:56 -0700

    Add basic script to fetch database from notion page

---
+++ src/lib/getTableData.ts
@@ -0,0 +1,53 @@
+import { getNotionClient, getDatabaseId } from './notionClient'
+import {
+  GetDatabaseResponse,
+  QueryDatabaseResponse,
+} from '@notionhq/client/build/src/api-endpoints'
+
+// Get metadata of the database eg. title, date
+// returns the database object as response
+export async function getTableHeader(): Promise<GetDatabaseResponse> {
+  try {
+    const databaseId = getDatabaseId()
+    const notion = getNotionClient()
+    const response = await notion.databases.retrieve({
+      database_id: databaseId,
+    })
+    return response
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+type tableProps = {
+  includeDraft?: boolean
+}
+
+// takes props for filtering options
+// returns contents in the database
+// by default, it does not include the draft posts
+export async function getTableContent(
+  props: tableProps = { includeDraft: false }
+): Promise<QueryDatabaseResponse> {
+  try {
+    const { includeDraft } = props
+    const databaseId = getDatabaseId()
+    const notion = getNotionClient()
+
+    const queryObj = {
+      database_id: databaseId,
+      ...(!includeDraft && {
+        filter: {
+          property: 'draft',
+          checkbox: {
+            does_not_equal: true,
+          },
+        },
+      }),
+    }
+    const response = await notion.databases.query(queryObj)
+    return response
+  } catch (error) {
+    console.error(error)
+  }
+}
---
+++ src/lib/notionClient.ts
@@ -0,0 +1,41 @@
+import { Client, LogLevel } from '@notionhq/client'
+
+export function getNotionClient(): Client {
+  const notion_token = import.meta.env.NOTION_API_KEY
+
+  // Throw an error if NOTION_API_KEY is not provided
+  if (!notion_token) {
+    throw new Error(
+      `NOTION_API_KEY not found: NOTION_API_KEY must be set in the .env file`
+    )
+  }
+
+  // initialize new Client object provided by Notion
+  // NOTION_API_KEY is required
+  const notion = new Client({
+    auth: notion_token,
+    logLevel: LogLevel.DEBUG, // for debugging
+  })
+
+  return notion
+}
+
+export function getDatabaseId(): string {
+  let databaseId = import.meta.env.NOTION_DATABASE_ID
+
+  // Throw an error if NOTION_DATABASE_ID is not provided
+  if (!databaseId) {
+    throw new Error(
+      `NOTION_DATABASE_ID not found: NOTION_DATABASE_ID must be set in the .env file`
+    )
+  }
+
+  // Throw an error if NOTION_DATABASE_ID is not 32 characters long
+  if (databaseId.length !== 32) {
+    throw new Error(
+      `Invalid length of NOTION_DATABASE_ID: NOTION_DATABASE_ID must be 32 characters long`
+    )
+  }
+
+  return databaseId
+}
