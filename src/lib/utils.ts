4c8220e90a703d08148b0d44cd4e9677c8d4a404
jcha0713 <joocha0713@gmail.com>
2022-05-22 23:48:07 -0700

    Download user uploaded images and displays them

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,6 @@
+import * as fs from 'fs';
+import * as https from 'https';
+
 export function getClassAttribute(block, objType) {
   let textObjs = block[block.type][objType];
 
@@ -75,4 +78,37 @@ export function getCorrectTagName(block, type: string): string {
     },
   };
   return correctTagName[type]();
+}
+
+export async function downloadFile(url, targetFile) {
+  if (!fs.existsSync(targetFile)) {
+    return await new Promise((resolve, reject) => {
+      https
+        .get(url, (response) => {
+          const code = response.statusCode ?? 0;
+
+          if (code >= 400) {
+            return reject(new Error(response.statusMessage));
+          }
+
+          // handle redirects
+          if (code > 300 && code < 400 && !!response.headers.location) {
+            return downloadFile(response.headers.location, targetFile);
+          }
+
+          // save the file to disk
+          const fileWriter = fs
+            .createWriteStream(targetFile)
+            .on('finish', () => {
+              resolve({});
+            });
+
+          response.pipe(fileWriter);
+        })
+        .on('error', (error) => {
+          reject(error);
+        });
+    });
+  }
+  return null;
 }
